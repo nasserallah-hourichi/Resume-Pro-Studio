3b5d930a24fe01182d6a596e4400dbb0d8a0fd9d
nasserallah hourichi <nasserallah.hourichi@gmail.com>
2026-07-03 10:28:44 +0100

    updated keys and values

--- supabase-config.js
+++ supabase-config.js
@@ -1,6 +1,6 @@
 window.SUPABASE_CONFIG = {
-    url: "https://YOUR_PROJECT_ID.supabase.co",
-    anonKey: "YOUR_SUPABASE_ANON_KEY",
+    url: "https://ljqdjnqmptubwylpesae.supabase.co",
+    anonKey: "**9MvNktqPS#M2J",
     redirectTo: window.location.origin === "null"
         ? ""
         : `${window.location.origin}${window.location.pathname}`
